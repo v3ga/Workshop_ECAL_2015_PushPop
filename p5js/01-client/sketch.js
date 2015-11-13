3b51d511584068475f417798bd03bb8a4d5c4422
v3ga <julien@v3ga.net>
2015-11-13 18:18:46 +0100

    updates

--- p5js/01-client/sketch.js
+++ p5js/01-client/sketch.js
@@ -1,24 +1,26 @@
 var websocket;
 
-function setup() {
-  // uncomment this line to make the canvas the full size of the window
+// --------------------------------------------
+function setup()
+{
   	createCanvas(500, 500);
 	websocket = new WebSocket("ws://MacBook-Pro-de-Julien.local:12345/p5websocket");
-
-   
 }
 
-function draw() {
-  // draw stuff here
+// --------------------------------------------
+function draw()
+{
   ellipse(width/2, height/2, 50, 50);
 }
 
-function mouseDragged() {
-  // Make a little object with mouseX and mouseY
+// --------------------------------------------
+function mouseDragged()
+{
   var data = {
     x: mouseX,
     y: mouseY
   };
+  
   // Send that object to the socket
   websocket.send('Ping');
 }
